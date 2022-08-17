a1833c22f83b473b3a224e53e245ee2998c49590
ahmed <ahmedthurwat@gmail.com>
2022-08-17 15:06:36 +0200

    finish tasbeh

--- lib/home/sebha/SebhaTab.dart
+++ lib/home/sebha/SebhaTab.dart
@@ -1,10 +1,110 @@
-import 'package:flutter/material.dart';
+import 'dart:math' as maths;
 
-class SebhaTab extends StatelessWidget {
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:islamy2022/main.dart';
+
+class SebhaTab extends StatefulWidget {
+  @override
+  State<SebhaTab> createState() => _SebhaTabState();
+}
+
+class _SebhaTabState extends State<SebhaTab> {
+  int counter = 0;
+  List<String> tasbehlist = [
+    'سبحان الله',
+    'الحمد لله ',
+    'الله اكبر ',
+    'استغفر الله العظيم '
+  ];
+  int currentindex = 0;
+  double angle = 0;
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.orange,
+      width: double.infinity,
+      child: Column(
+        children: [
+          Stack(
+            alignment: AlignmentDirectional.topCenter,
+            children: [
+              InkWell(
+                onTap: clickshephacounter,
+                child: Container(
+                  margin: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height * 0.1),
+                  child: Transform.rotate(
+                      angle: maths.pi / 180 * angle,
+                      child:
+                          Image.asset('assets/images/body of seb7a-logo.png')),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                    left: MediaQuery.of(context).size.height * 0.06),
+                child: Image.asset('assets/images/head of seb7a_logo.png'),
+              ),
+            ],
+          ),
+          Expanded(
+            child: Column(
+              children: [
+                Container(
+                  child: Text(
+                    'عدد التسبيحات',
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  padding: EdgeInsets.all(25),
+                  decoration: BoxDecoration(
+                    color: Mythemedata.primaryColor,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Text(
+                    '$counter',
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Container(
+                  padding: EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                    color: Mythemedata.primaryColor,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Text(
+                    tasbehlist[currentindex],
+                    style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
+  }
+
+  void clickshephacounter() {
+    setState(() {
+      counter++;
+      angle += 30;
+      if (counter % 33 == 0) {
+        if (currentindex == 3) {
+          currentindex = 0;
+        }
+        currentindex++;
+      }
+    });
   }
 }
